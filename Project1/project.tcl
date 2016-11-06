23dbf0251e41cc472e99ae38f0cf43903ce86231
teresalazar13 <teresa.sal13@gmail.com>
2016-11-06 16:22:23 +0000

    Cenario 2.

--- Project1/project.tcl
+++ Project1/project.tcl
@@ -1,10 +1,10 @@
 if {$argc == 2} {
     set cenario [lindex $argv 0]
-    set protocol "tcp"
-    set window [lindex $argv 1]
-} elseif {$argc == 1} {
+    set protocol [lindex $argv 1]
+} elseif {$argc == 3} {
     set cenario [lindex $argv 0]
-    set protocol "udp"
+    set protocol [lindex $argv 1]
+    set window [lindex $argv 2]
 } else {
     puts "Error"
     puts "Usage: $argv0 cenario"
@@ -16,7 +16,10 @@ if {$argc == 2} {
 # Create the 'Simulator' object
 set ns [new Simulator]
 
-$ns color 1 Pink
+$ns color 1 Red
+$ns color 2 Blue
+$ns color 3 Orange
+$ns color 4 Pink
 
 # Open a file for writing the nam trace data
 set nf [open out.nam w]
@@ -66,24 +69,53 @@ $cbr0 set maxpkts_ 1
 set null0 [new Agent/Null]
 $ns attach-agent $n7 $null0
 
-if {$cenario == 1} {
-  if {$protocol == "udp"} {
-    set udp0 [new Agent/UDP]
-    $ns attach-agent $n0 $udp0
-    $cbr0 attach-agent $udp0
-    $ns connect $udp0 $null0
-    $udp0 set class_ 1
-  }
-  if {$protocol == "tcp"} {
-    set tcp0 [$ns create-connection TCP $n0 TCPSink $n7 2]
-    $tcp0 set window_ $window
-    $ns attach-agent $n0 $tcp0
-    $cbr0 attach-agent $tcp0
-  }
+if {$protocol == "udp"} {
+  set udp0 [new Agent/UDP]
+  $ns attach-agent $n0 $udp0
+  $cbr0 attach-agent $udp0
+  $ns connect $udp0 $null0
+  $udp0 set class_ 1
+}
+if {$protocol == "tcp"} {
+  set tcp0 [$ns create-connection TCP $n0 TCPSink $n7 2]
+  $tcp0 set window_ $window
+  $ns attach-agent $n0 $tcp0
+  $cbr0 attach-agent $tcp0
 }
 
 if {$cenario == 2} {
+  set udp1 [new Agent/UDP]
+  $ns attach-agent $n1 $udp1
 
+  set cbr1 [new Application/Traffic/CBR]
+  $cbr1 set rate_ 6Mb
+  $cbr1 attach-agent $udp1
+
+  set null1 [new Agent/Null]
+  $ns attach-agent $n5 $null1
+  $ns connect $udp1 $null1
+
+  $udp1 set class_ 3
+
+  $ns at 0.5 "$cbr1 start"
+	$ns at 6.0 "$cbr1 stop"
+
+
+  set udp2 [new Agent/UDP]
+  $ns attach-agent $n2 $udp2
+
+  set cbr2 [new Application/Traffic/CBR]
+  $cbr2 set rate_ 5Mb
+  $cbr2 attach-agent $udp2
+
+  set null2 [new Agent/Null]
+  $ns attach-agent $n5 $null2
+  $ns connect $udp2 $null2
+
+  $udp2 set class_ 4
+
+  $ns at 0.5 "$cbr2 start"
+  $ns at 6.0 "$cbr2 stop"
 }
 
 $ns at 0.5 "$cbr0 start"
