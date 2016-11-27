7ac9659b01d2bc813949f6f102ed6ca3ad921c7f
teresalazar13 <teresa.sal13@gmail.com>
2016-11-27 19:04:37 +0000

    Worksheet 6.

---
+++ Worksheet6/client2.c
@@ -0,0 +1,50 @@
+/*************************************************************
+* CLIENTE liga ao servidor (definido em argv[1]) no porto especificado
+* (em argv[2]), escrevendo a palavra predefinida (em argv[3]).
+* USO: >cliente <enderecoServidor> <porto> <Palavra>
+*************************************************************/
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+
+void erro(char *msg);
+
+int main(int argc, char *argv[]) {
+  char endServer[100];
+  int fd;
+  struct sockaddr_in addr;
+  struct hostent *hostPtr;
+
+  if (argc != 3) {
+    printf("cliente <host> <port>\n");
+    exit(-1);
+  }
+  strcpy(endServer, argv[1]);
+
+  if ((hostPtr = gethostbyname(endServer)) == 0) // Converte nome para endereço
+    erro("Nao consegui obter endereço");
+
+  bzero((void *) &addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = ((struct in_addr *) (hostPtr->h_addr))->s_addr;
+  addr.sin_port = htons((short) atoi(argv[2]));
+
+  if((fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) // Cria um novo socket
+    erro("socket");
+  if(connect(fd, (struct sockaddr *) &addr, sizeof (addr)) < 0) // Inicia uma ligação num socket
+    erro("Connect");
+
+  close(fd);
+  exit(0);
+}
+
+void erro(char *msg) {
+  printf("Erro: %s\n", msg);
+  exit(-1);
+}
---
+++ Worksheet6/server.c
@@ -0,0 +1,72 @@
+/*******************************************************************
+* SERVIDOR no porto 9000, à escuta de novos clientes. Quando surjem
+* novos clientes os dados por eles enviados são lidos e descarregados no ecra
+*******************************************************************/
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+
+#define SERVER_PORT 9000
+#define BUF_SIZE 1024
+
+void process_client(int fd);
+void erro(char *msg);
+
+int main() {
+  int fd, client;
+  struct sockaddr_in addr, client_addr;
+  int client_addr_size;
+
+  bzero((void *) &addr, sizeof(addr));
+  addr.sin_family = AF_INET; // Domínio no qual o socket será usado
+  addr.sin_addr.s_addr = htonl(INADDR_ANY); // Qualquer endereço do host
+  addr.sin_port = htons(SERVER_PORT); // Porto a associar
+
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) // Cria um novo socket
+    erro("na funcao socket");
+  if (bind(fd, (struct sockaddr*) &addr, sizeof(addr)) < 0) // Associa um socket a um determinado endereço
+    erro("na funcao bind");
+  if(listen(fd, 5) < 0) // Aguarda pela recepção de ligações
+    erro("na funcao listen");
+
+  while (1) {
+    client_addr_size = sizeof(client_addr);
+    client = accept(fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_size); // Aceita uma ligação
+    if (client > 0) {
+      if (fork() == 0) {
+        close(fd);
+        process_client(client);
+        exit(0);
+      }
+      close(client);
+    }
+  }
+  return 0;
+}
+
+void process_client(int client_fd) {
+  int nread = 0;
+  char buffer[BUF_SIZE];
+  char temp[BUF_SIZE];
+  char invertedBuffer[BUF_SIZE];
+  nread = read(client_fd, buffer, BUF_SIZE-1);
+  buffer[nread] = '\0';
+  for (int i = 0; i < strlen(buffer); i++) {
+    invertedBuffer[strlen(buffer) - i - 1] = buffer[i];
+  }
+  write(client_fd, invertedBuffer, sizeof(invertedBuffer));
+  printf("%s\n", invertedBuffer);
+  fflush(stdout);
+  close(client_fd);
+}
+
+void erro(char *msg) {
+  printf("Erro: %s\n", msg);
+  exit(-1);
+}
