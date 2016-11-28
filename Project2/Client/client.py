3218e74729c59e2a4b48b376ec07a38038e7272c
teresalazar13 <teresa.sal13@gmail.com>
2016-11-28 13:42:42 +0000

    Starts Project2. Adds sockets in client and server. Adds menu in client.

---
+++ Project2/Client/client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python           # This is client.py file
+
+import socket               # Import socket module
+import sys
+
+
+def create_socket(port):
+    try:
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as e:
+        print(e)
+        sys.exit(1)
+    host = socket.gethostname()
+    try:
+        client_socket.connect((host, port))
+    except socket.error as e:
+        print(e)
+        sys.exit(1)
+    print("Socket created succefully")
+    return client_socket
+
+
+def main():
+    users_and_files = {}
+    loggedin_user = []
+    server_port = 9000
+    conn = create_socket(server_port)
+
+    while True:
+        choice = menu()
+
+
+def menu():
+    option = input("1 LIST_MESS - para listar todas as mensagens por ler.\n2 LIST_USERS - para listar todos os clientes autorizados.\n3 SEND_MESS - para enviar uma mensagem para um cliente (autorizado).\n4 LIST_READ - para listar todas as mensagens ja lidas.\n5 REMOVE_MES - para apagar mensagens.\n6 CHANGE_PASSW - alterar a password\n7 OPER - para o cliente obter os privilegios do operador.\n8 QUIT - para o cliente abandonar o sistema.\n")
+    return option
+
+
+if __name__ == '__main__':
+    main()
---
+++ Project2/Server/server.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python           # This is server.py file
+
+import socket               # Import socket module
+
+
+def create_socket(port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    s.bind((host, port))
+    s.listen(5)
+    print("Socket creation sucefull")
+    return s
+
+
+def server(port):
+    server_socket = create_socket(port)
+    print "The server is ready to receive"
+
+    while True:
+        print "Connected"
+
+
+def main():
+    print "Hello World"
+    server(9000)
+
+
+if __name__ == '__main__':
+    main()
